fbb3ce9f16cd9d6c4c6e9c11c864c2bd16f782f8
Mustafa ÖZTÜRK <bay.mustafa.37@hotmail.com>
2022-07-14 20:21:09 +0300

    Initial Commit

---
+++ bookLibrary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bookLibrary
+//
+//  Created by Mustafa  on 14.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bookLibrary/bookLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookLibraryApp.swift
+//  bookLibrary
+//
+//  Created by Mustafa  on 14.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct bookLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
